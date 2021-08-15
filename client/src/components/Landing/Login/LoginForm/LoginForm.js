956bcfd8c96a88aecaea2b7b785858e6b8388674
Severus7 <joshuasapz@gmail.com>
2021-08-15 14:47:29 +0800

    Refactored LoginForm

--- client/src/components/Landing/Login/LoginForm/LoginForm.js
+++ client/src/components/Landing/Login/LoginForm/LoginForm.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './LoginForm.module.css';
 
 const LoginForm = () => {
+
+    // const inputState = useState({ title: '', password: ''});
+    const [inputEmail, setInputEmail] = useState('');
+    const [inputPassword, setInputPassword] = useState('');
+
     return (
-        <React.Fragment>
-            <form className={classes.LoginForm}>
-                <div>SmartCT Logo</div>
-                <div className={classes.LoginCredentials}>
-                    {/* <label>Email address</label> */}
-                    <input type="email" placeholder="Email address" className={classes.Input} />
-                    {/* <input type="password" placeholder="Password" /> */}
-                </div>
-                <div className={classes.LoginCredentials}>
-                    {/* <label>Password</label> */}
-                    <input type="password" placeholder="Password" className={classes.Input} />
-                </div>
-                <div className={classes.ButtonDiv}>
-                    <button className={classes.Button}>Login</button>
-                </div>
-                <div className={classes.HyperlinkDiv}>
-                    <a href="/forgot-password" className={classes.Hyperlink}>Forgot Password</a>
-                </div>
-                <div className={classes.HyperlinkDiv}>
-                    <a href="/create-account" className={classes.Hyperlink}>Register</a>
-                </div>
-            </form>
-        </React.Fragment>
+      <React.Fragment>
+        <form className={classes.LoginForm}>
+          <div>SmartCT Logo</div>
+          <div className={classes.LoginCredentials}>
+            {/* <label>Email address</label> */}
+            <input
+              type="email"
+              placeholder="Email address"
+              className={classes.Input}
+              id="email"
+              value={inputEmail}
+              onChange={(event) => {
+                setInputEmail(event.target.value);
+              }}
+            />
+            {/* <input type="password" placeholder="Password" /> */}
+          </div>
+          <div className={classes.LoginCredentials}>
+            {/* <label>Password</label> */}
+            <input
+              type="password"
+              placeholder="Password"
+              className={classes.Input}
+              id="password"
+              value={inputPassword}
+              onChange={(event) => {
+                setInputPassword(event.target.value);
+              }}
+            />
+          </div>
+          <div className={classes.ButtonDiv}>
+            <button className={classes.Button}>Login</button>
+          </div>
+          <div className={classes.HyperlinkDiv}>
+            <a href="/forgot-password" className={classes.Hyperlink}>
+              Forgot Password
+            </a>
+          </div>
+          <div className={classes.HyperlinkDiv}>
+            <a href="/create-account" className={classes.Hyperlink}>
+              Register
+            </a>
+          </div>
+        </form>
+      </React.Fragment>
     );
 }
 
