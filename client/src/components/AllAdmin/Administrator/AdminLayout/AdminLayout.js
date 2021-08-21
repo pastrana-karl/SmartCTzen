44ed7f7615a9776070bb23c95ddb7bc4ad3a1dcc
Severus7 <joshuasapz@gmail.com>
2021-08-21 10:54:35 +0800

    Fixing the navbar for ADMIN

--- client/src/components/AllAdmin/Administrator/AdminLayout/AdminLayout.js
+++ client/src/components/AllAdmin/Administrator/AdminLayout/AdminLayout.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import Toolbar from '../../../Navigation/Toolbar/Toolbar';
+import NavigationItems from '../../../UI/NavigationItems/NavigationItems';
+import NavigationItem from '../../../Navigation/NavigationItems/NavigationItem/NavigationItem';
 
 const AdminLayout = () => (
     <React.Fragment>
-        <Toolbar />
+        <Toolbar>
+            <NavigationItems>
+                <NavigationItem link="/" exact>Home</NavigationItem>
+                <NavigationItem link="/smartctzens-features">Features</NavigationItem>
+                <NavigationItem link="/smartctzens-login">Login</NavigationItem>
+            </NavigationItems>
+        </Toolbar>
     </React.Fragment>
 );
 
---
+++ client/src/components/UI/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+import classes from './NavigationItems.module.css';
+
+const NavigationItems = ( props ) => (
+    <ul className={classes.NavigationItems}>{props.children}</ul>
+);
+
+export default NavigationItems;
