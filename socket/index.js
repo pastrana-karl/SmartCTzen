79be953f38849f8f8a8355587b01fcafdb0d44a2
Severus7 <joshuasapz@gmail.com>
2021-09-13 02:35:48 +0800

    socketio

--- socket/index.js
+++ socket/index.js
@@ -6,8 +6,35 @@ const io = require("socket.io")(8900, {
 
 let users = [];
 
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId);
+}
+
 io.on("connection", (socket) => {
     console.log("A user connected");
     io.emit("Welcome", "Hello this is socket server ");
+
     //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text
+        });
+    });
+
+    //when disconnect
+
+
 });
